02d1855beb48efc0d1ddefa46aa165711a8608b3
guzhongzhi <guzhongzhi@qq.com>
2017-10-14 23:49:46 +0800

    form

---
+++ mock/MemberTypes.js
@@ -0,0 +1,17 @@
+module.exports = {
+  api: '/api/member/types',
+  data: [
+    {
+      label: '用户',
+      value: 1
+    },
+    {
+      label: '商家',
+      value: 1
+    },
+    {
+      label: '员工',
+      value: 1
+    }
+  ]
+}
---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CommonFooter',
+    data () {
+      return {}
+    }
+  }
+</script>
